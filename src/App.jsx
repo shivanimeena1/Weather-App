13846ff731c7d002b82fea9338931580b10a25c6
shivanimeena1 <153616308+shivanimeena1@users.noreply.github.com>
2024-09-25 12:08:50 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import WeatherApp from "./WeatherApp";
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        <WeatherApp></WeatherApp>
+      </div>
+    </>
+  );
+}
+
+export default App;
